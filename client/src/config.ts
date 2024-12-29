6033ddca7213430e6c7db299feb159e491e60ca5
Hrishikesh Kalita <95520030+HrishiD89@users.noreply.github.com>
2024-12-30 00:16:35 +0530

    updated app_url

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
 // export const BACKEND_URL = "http://localhost:3001/api/v1"
 export const BACKEND_URL = "https://brainly-be-ej66.onrender.com/api/v1/"
 // export const APP_URL = "http://localhost:5173";
-export const APP_URL = "https://brainly-full-app.vercel.app/";
+export const APP_URL = "https://brainly-full-app.vercel.app";
